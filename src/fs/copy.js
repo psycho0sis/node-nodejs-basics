6c070ec8a0a952b8ac329fe2075b410cba555a0d
ilonaK <ikosko@alfabank.ru>
2022-11-25 00:34:07 +0300

    feat: add solutions to create.js, copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,28 @@
+import path from "path";
+import fs from "fs";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const copy = async () => {
-    // Write your code here 
+  const pathToTheFolder = path.join(__dirname, "/files");
+  const pathToDestinationFolder = path.join(__dirname, "/files_copy");
+
+  fs.access(pathToDestinationFolder, (err, data) => {
+    if (!err) throw new Error("FS operation failed");
+  });
+
+  fs.cp(
+    pathToTheFolder,
+    pathToDestinationFolder,
+    { recursive: true },
+    (err) => {
+      if (err) {
+        console.error(err);
+      }
+    }
+  );
 };
 
 copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,22 @@
+import path from "path";
+import fs from "fs";
+import { Buffer } from "node:buffer";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const create = async () => {
-    // Write your code here 
+  const pathToTheFile = path.join(__dirname, "/files", "fresh.txt");
+  const data = new Uint8Array(Buffer.from("I am fresh and young"));
+
+  fs.readFile(pathToTheFile, (err, data) => {
+    if (!err) throw new Error("FS operation failed");
+  });
+
+  fs.writeFile(pathToTheFile, data, (err) => {
+    if (err) throw err;
+  });
 };
 
 await create();
