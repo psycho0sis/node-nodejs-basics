0fcdd425219faacb08ba6dfdb46620a8009d7f16
ilonaK <ikosko@alfabank.ru>
2022-11-25 17:34:38 +0300

    feat: add rename.js solution

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,24 @@
+import path from "path";
+import { readFile, rename as fsRename } from "fs";
+
+import { __filename, __dirname } from "./constants.js";
+
 const rename = async () => {
-    // Write your code here 
+  const pathToTheWrongFile = path.join(__dirname, "/files/wrongFilename.txt");
+  const pathToTheCorrectFile = path.join(__dirname, "/files/properFilename.md");
+
+  readFile(pathToTheWrongFile, (err, data) => {
+    if (err) throw new Error("FS operation failed");
+  });
+
+  readFile(pathToTheCorrectFile, (err, data) => {
+    if (!err) throw new Error("FS operation failed");
+  });
+
+  fsRename(pathToTheWrongFile, pathToTheCorrectFile, function (err) {
+    if (err) throw err;
+    console.log("File Renamed.");
+  });
 };
 
 await rename();
