6397b0f7b287638ee8cfdc0d4d5d3734622111c7
ilonaK <ikosko@alfabank.ru>
2022-11-25 17:34:59 +0300

    feat: add ./constants.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,28 +1,21 @@
 import path from "path";
-import fs from "fs";
-import { fileURLToPath } from "url";
+import { access, cp } from "fs";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import { __filename, __dirname } from "./constants.js";
 
 const copy = async () => {
   const pathToTheFolder = path.join(__dirname, "/files");
   const pathToDestinationFolder = path.join(__dirname, "/files_copy");
 
-  fs.access(pathToDestinationFolder, (err, data) => {
+  access(pathToDestinationFolder, (err) => {
     if (!err) throw new Error("FS operation failed");
   });
 
-  fs.cp(
-    pathToTheFolder,
-    pathToDestinationFolder,
-    { recursive: true },
-    (err) => {
-      if (err) {
-        console.error(err);
-      }
+  cp(pathToTheFolder, pathToDestinationFolder, { recursive: true }, (err) => {
+    if (err) {
+      console.error(err);
     }
-  );
+  });
 };
 
 copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,20 +1,18 @@
 import path from "path";
-import fs from "fs";
+import { readFile, writeFile } from "fs";
 import { Buffer } from "node:buffer";
-import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import { __filename, __dirname } from "./constants.js";
 
 const create = async () => {
   const pathToTheFile = path.join(__dirname, "/files", "fresh.txt");
   const data = new Uint8Array(Buffer.from("I am fresh and young"));
 
-  fs.readFile(pathToTheFile, (err, data) => {
+  readFile(pathToTheFile, (err) => {
     if (!err) throw new Error("FS operation failed");
   });
 
-  fs.writeFile(pathToTheFile, data, (err) => {
+  writeFile(pathToTheFile, data, (err) => {
     if (err) throw err;
   });
 };
