890eb7d77862d79d9ed27219a5ce7304fc7d1559
ilonaK <ikosko@alfabank.ru>
2022-11-26 23:26:02 +0300

    feat: add streams/read.js solution

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,19 @@
+import path from "path";
+import { createReadStream } from "fs";
+import { fileURLToPath } from "url";
+
+export const __filename = fileURLToPath(import.meta.url);
+export const __dirname = path.dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+  const pathToTheFile = path.join(__dirname, "/files/fileToRead.txt");
+  const stream = createReadStream(pathToTheFile, "utf8");
+
+  stream.on("data", (data) => {
+    data = data.toString();
+    process.stdout.write(data.toString() + "\n");
+  });
+  stream.on("error", (err) => console.log(`Err: ${err}`));
 };
 
 await read();
