1aeb9d64edc6800ce8d71540e268833088c77c84
ilonaK <ikosko@alfabank.ru>
2022-11-26 18:54:05 +0300

    feat: add hash/calcHash.js solution

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,21 @@
+import path from "path";
+import { readFile } from "fs";
+import { createHash } from "crypto";
+import { fileURLToPath } from "url";
+
+export const __filename = fileURLToPath(import.meta.url);
+export const __dirname = path.dirname(__filename);
+
 const calculateHash = async () => {
-    // Write your code here 
+  const pathToTheFile = path.join(
+    __dirname,
+    "/files/fileToCalculateHashFor.txt"
+  );
+
+  readFile(pathToTheFile, (err, data) => {
+    if (err) throw err;
+    console.log(createHash("sha256").update(data.toString()).digest("hex"));
+  });
 };
 
 await calculateHash();
