6c590206710a8bc3f65e931b003c9cbc121ba49a
ilonaK <ikosko@alfabank.ru>
2022-11-28 22:42:19 +0300

    feat: add streams/transform.js solution

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,28 @@
+import path from "path";
+import { Transform, pipeline } from "stream";
+import { createWriteStream, createReadStream } from "fs";
+
+import { __dirname } from "./constants.js";
+
 const transform = async () => {
-    // Write your code here 
+  const pathToTheFileForReading = path.join(__dirname, "/files/fileToRead.txt");
+  const pathToTheFileForWriting = path.join(
+    __dirname,
+    "/files/fileToWrite.txt"
+  );
+
+  const revert = new Transform({
+    transform(chunk, encoding, callback) {
+      const data = chunk.toString().split("").reverse().join("");
+      callback(null, data);
+    },
+  });
+
+  pipeline(process.stdin, revert, process.stdout, (err) => {
+    throw new Error(err);
+  });
+
+  console.log("\nWrite smth ..\n");
 };
 
 await transform();
