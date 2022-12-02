5e5705f096099eb06046ac2f246246ba408c3597
ilonaK <ikosko@alfabank.ru>
2022-12-02 19:14:36 +0300

    feat: add cp/cp.js solution

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,16 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
+import { fork } from "child_process";
+import { fileURLToPath } from "url";
+import path from "path";
+
+export const __filename = fileURLToPath(import.meta.url);
+export const __dirname = path.dirname(__filename);
+
+const spawnChildProcess = async (...args) => {
+  const child = fork(__dirname + "/files/script.js", args);
+
+  child.on("close", (code) =>
+    console.log(`Child process exited. Code: ${code}`)
+  );
 };
 
-spawnChildProcess();
+spawnChildProcess("--silent", "--all");
